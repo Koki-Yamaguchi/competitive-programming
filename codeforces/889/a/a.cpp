a10114f89c7186136b84647023a7a1bd87b64e85
Koki Yamaguchi <yk49at21@gmail.com>
2020-12-12 10:30:15 +0900

    Namida

---
+++ codeforces/889/a/a.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; (i) < (int) (n); (i) ++)
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define dump(x) cerr << #x << " = " << x << '\n';
+using ll = long long;
+template<typename T, typename U>
+ostream& operator << (ostream& os, const pair<T, U>& p) {
+        os << p.first << " " << p.second;
+        return os;
+}
+template<typename T>
+ostream& operator << (ostream& os, const vector<T>& vec) {
+	for (int i = 0; i < vec.size(); i ++) {
+		os << vec[i] << (i + 1 == vec.size() ? "" : " ");
+	}
+	return os;
+}
+
+void solve() {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        rep(i, n) {
+                cin >> a[i];
+                assert(a[i] <= i);
+        }
+        int t = 1;
+        int ans = 0;
+        set<int> st;
+        rep(i, n) {
+                if (st.count(a[i]) == 0) {
+                        ans ++;
+                } else {
+                        st.erase(a[i]);
+                }
+                st.insert(t);
+                t ++;
+        }
+        cout << ans << '\n';
+}
+
+int main() {
+        cin.tie(nullptr);
+        ios::sync_with_stdio(false);
+        // int t; cin >> t;
+        int t = 1;
+        while (t --) {
+                solve();
+        }
+        return 0;
+}
